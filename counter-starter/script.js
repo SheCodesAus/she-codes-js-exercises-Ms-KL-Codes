71a04ff54322775493fad7042bf3c237b6c63594
Ms-KL-Codes <124024982+Ms-KL-Codes@users.noreply.github.com>
2023-02-01 13:20:50 +0800

    Counter Challenge 3 (+ or - based on user input) complete
    added playground for trial and error

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -50,12 +50,12 @@
 // CHALLENGE:
 // 1 - use new function to add 2 new buttons for subtracting by 1 and 10 respectively
 
-function add_or_subtract(step) {
-    let numberElement = document.getElementById("number");
-    let number = numberElement.innerText;
-    number = parseInt(number) + step;
-    numberElement.innerText = number;
-    }
+// function add_or_subtract(step) {
+//     let numberElement = document.getElementById("number");
+//     let number = numberElement.innerText;
+//     number = parseInt(number) + step;
+//     numberElement.innerText = number;
+//     }
     
 // name changed to add_or_subtract to reflect accurate functionality
 
@@ -77,3 +77,21 @@ function division(step) {
     }
 
 // Challenge 3 - Try adding an input field, so instead of using your buttons, the user types in what number they want to add/subtract.
+
+function add_or_subtract() {
+    // INPUT NUMBER
+    let input_number= document.getElementById("input_number").value;
+    // above based on the showName function in playground
+
+    input_number = parseInt(input_number);
+    // input_number = parseInt(input_number).innerText;
+    // don't need inner_text as the value is not within HTML tags 
+        // eg:             <p id="number">0</p> --> 0 = inner_text
+    
+    // Change the value of the original number (eg 0)
+    // This will change based on input number above
+    let numberElement = document.getElementById("number");
+    let number = numberElement.innerText;
+    number = parseInt(number) + input_number;
+    numberElement.innerText = number;
+    }
