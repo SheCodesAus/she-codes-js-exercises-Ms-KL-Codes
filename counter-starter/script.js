acbb75faf3773552717beed454c69fd6013d72f8
Ms-KL-Codes <124024982+Ms-KL-Codes@users.noreply.github.com>
2023-02-06 10:18:40 +0800

    generate random number in counter. Use random number to add or subtract from number

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -95,3 +95,43 @@ function add_or_subtract() {
     number = parseInt(number) + input_number;
     numberElement.innerText = number;
     }
+
+// HOMEWORK CHALLENGE:  Add a button for adding or subtracting a random amount from the displayed number.
+
+function random_number_generator () {
+    // https://www.w3schools.com/js/js_random.asp
+    let random_numberElement = document.getElementById("random_number");
+        // name the element that is the current random_number by using ID to find it in html
+    // let random_number = Math.random();
+    let random_number = Math.floor(Math.random()*199) - 99;
+        // http://www.java2s.com/example/javascript/data-type/random-number-between-negative-and-positive-value.html
+        // https://gist.github.com/spyesx/bb1742fe863c0b8a0191
+            // floor = make integer
+            // random = decimal between 0 -1
+                // random eg 0.001 now = 0 due to floor 
+                // random eg 0.900 now = 1 due to floor
+            // * 199 = max random number is now 199
+                // 0 = min random number 
+                // eg 1 x 199 or 0 x 199
+            // - 99 = takes 99 off the result
+                // 100 = max random
+                // -99 = min random
+    random_numberElement.innerText = random_number;
+        // change the value of the element in the html variable to the new random number
+}
+
+function add_subtract_random() {
+    let random_numberElement = document.getElementById("random_number");
+    let random_number = random_numberElement.innerText;
+    // get the random_number that is in between the random_numberElement tags
+
+    let numberElement = document.getElementById("number");
+    let number = numberElement.innerText;
+    // get the number that is in between the numberElement tags
+
+    number = parseInt(number) + parseInt(random_number);
+    // convert the strings to integers and add them together
+
+    numberElement.innerText = number;
+    // replace the value in between the tags with the new number
+    }
